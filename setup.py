65da1a01fb658dec28aad08aa0aaeaf143fd3a4a
jagadishmali567 <jagadishmali567@gmail.com>
2025-07-12 16:49:43 +0530

    Add initial setup script for the AI-Powered Source Code Analysis Project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+# This is a setup script for a Python project that uses setuptools.
+setup(
+    name= "AI-Powered Source Code Analysis Project",
+    version= "0.1",
+    author= "Jagadish",
+    author_email= "jagadishmali567@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
